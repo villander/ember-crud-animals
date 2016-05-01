457a03e87a9a164c206f936fa363a3aea9c06c80
Michael Villander <nousvillander@gmail.com>
2016-05-01 01:42:41 -0300

    Add unit test of fullName toanimal model

--- tests/unit/models/animal-test.js
+++ tests/unit/models/animal-test.js
@@ -1,8 +1,13 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('animal', 'Unit | Model | animal', {
   // Specify the other units that are required for this test.
   needs: []
+});
+
+let fullName = Ember.computed('firstName', 'lastName', function() {
+  return this.get('firstName') + ' ' + this.get('lastName');
 });
 
 test('it has the attributes: firstName and lastName', function(assert) {
@@ -11,4 +16,21 @@ test('it has the attributes: firstName and lastName', function(assert) {
   const hasAttrLastName = Object.keys(model.toJSON()).indexOf('lastName') > -1;
   assert.ok(hasAttrFirstName);
   assert.ok(hasAttrLastName);
+});
+
+test('fullName', function(assert){
+  let animal = this.subject({firstName: 'Cavalo', lastName: 'Marinho', fullName});
+  assert.equal(animal.get('fullName'), 'Cavalo Marinho');
+
+  Ember.run(function() {
+    animal.set('firstName', 'Macaco');
+  });
+
+  assert.equal(animal.get('fullName'), 'Macaco Marinho');
+
+  Ember.run(function() {
+    animal.set('lastName', 'Prego');
+  });
+
+  assert.equal(animal.get('fullName'), 'Macaco Prego');
 });
