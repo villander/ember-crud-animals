95124e8de01a8795eeb8fc49d563bf1ea3daf3eb
Michael Villander <nousvillander@gmail.com>
2016-11-10 17:33:53 -0200

    Add ember-offline

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  init() {
+    this._super(...arguments);
+    window.setInterval(this.checking, 3000);
+  },
+  checking() {
+    $.getJSON('/ajax_test');
+  },
   afterModel: function () {
     this.transitionTo('animals');
   },
